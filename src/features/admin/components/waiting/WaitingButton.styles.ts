f4d3539326d1af4110ddd27636a8580ffe3453d4
SeHyun <rla6170@naver.com>
2025-05-15 09:48:58 +0900

    feat: add waiting interaction component UI (button, modal)

---
+++ src/features/admin/components/waiting/WaitingButton.styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+export const Container = styled(motion.button)`
+  width: 100%;
+  padding: 0.8125rem 1.25rem;
+  display: flex;
+  align-items: center;
+  border-radius: 0.75rem;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  gap: 0.87rem;
+`;
+
+export const InnerButton = styled.div`
+  display: flex;
+  padding: 0.25rem;
+  align-items: center;
+  gap: 0.625rem;
+  width: 2rem;
+  height: 2rem;
+  background-color: ${(props) => props.theme.colors.primary.bl400};
+  border-radius: 50%;
+`;
+
+export const Text = styled.p`
+  color: ${(props) => props.theme.colors.grayScale.white};
+  ${(props) => props.theme.fonts.header.h4};
+`;
