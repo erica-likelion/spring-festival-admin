e409d80bd302bcd7b8ff317c8a0ac4860f94845d
SeHyun <rla6170@naver.com>
2025-05-15 11:17:54 +0900

    fix: button text overflow

--- src/components/button/AlarmButton.styles.ts
+++ src/components/button/AlarmButton.styles.ts
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 export const Container = styled(motion.button)<{ $size: 'small' | 'large'; disabled?: boolean }>`
   display: flex;
   width: ${(props) => (props.$size === 'large' ? '4.5rem' : '3.75rem')};
-  padding: ${(props) => (props.$size === 'large' ? '0.5rem' : '0.5625rem')};
+  padding: ${(props) => (props.$size === 'large' ? '0.5rem 0rem' : '0.5625rem 0rem')};
   flex-direction: column;
   justify-content: center;
   align-items: center;
@@ -14,7 +14,8 @@ export const Container = styled(motion.button)<{ $size: 'small' | 'large'; disab
     props.disabled ? props.theme.colors.grayScale.black : props.theme.colors.grayScale.gy900};
   border-radius: 0.375rem;
   border: none;
-  ${(props) => props.theme.fonts.body.xsmall500}
+  ${(props) =>
+    props.$size === 'large' ? props.theme.fonts.body.xsmall500 : props.theme.fonts.body.xsmall400};
   color: ${(props) =>
     props.disabled ? props.theme.colors.grayScale.gy800 : props.theme.colors.grayScale.white};
 
