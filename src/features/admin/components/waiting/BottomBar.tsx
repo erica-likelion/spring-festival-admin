6c81d6fcaab9af398fdf7f1d4452ed39834f4715
SeHyun <rla6170@naver.com>
2025-05-16 13:26:20 +0900

    feat: add toast feature button

--- src/features/admin/components/waiting/BottomBar.tsx
+++ src/features/admin/components/waiting/BottomBar.tsx
@@ -2,8 +2,22 @@ import { AnimatePresence } from 'framer-motion';
 import * as S from './BottomBar.styles';
 import DeleteIcon from '@/assets/icons/nrk_trash.svg?react';
 import CheckIcon from '@/assets/icons/nrk_check.svg?react';
+import useToast from '@/hooks/useToast';
 
-export default function BottomBar({ id }: { id: number | null }) {
+export default function BottomBar({
+  id,
+  setSelectedIndex,
+}: {
+  id: number;
+  setSelectedIndex: () => void;
+}) {
+  const { open } = useToast();
+
+  const handleClick = (message: string) => {
+    open(message, 3000, 'bootom-bar' + id);
+    console.log(message);
+    setSelectedIndex();
+  };
   if (!id) return null;
   return (
     <AnimatePresence>
@@ -25,11 +39,17 @@ export default function BottomBar({ id }: { id: number | null }) {
             <S.Text>선택</S.Text>
           </S.TextSection>
           <S.ButtonSection>
-            <S.Button whileTap={{ scale: 0.97, backgroundColor: '#212526' }}>
+            <S.Button
+              whileTap={{ scale: 0.97, backgroundColor: '#212526' }}
+              onClick={handleClick.bind(null, '노쇼 처리됐어요')}
+            >
               <DeleteIcon width={'1.5rem'} height={'1.5rem'} />
               <S.ButtonText>노쇼/삭제</S.ButtonText>
             </S.Button>
-            <S.Button whileTap={{ scale: 0.97, backgroundColor: '#212526' }}>
+            <S.Button
+              whileTap={{ scale: 0.97, backgroundColor: '#212526' }}
+              onClick={handleClick.bind(null, '입장 완료 처리됐어요')}
+            >
               <CheckIcon width={'1.5rem'} height={'1.5rem'} />
               <S.ButtonText>입장 완료</S.ButtonText>
             </S.Button>
